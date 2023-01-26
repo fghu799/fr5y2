a4ed5fa9340459fe97d346a61f7cf2fc22233721
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-01-26 19:54:25 +0900

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [solidJs(), tailwind()],
-  adapter: vercel()
+  integrations: [solidJs(), tailwind()]
 });
